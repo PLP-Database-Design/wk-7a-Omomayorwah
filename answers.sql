fa80d3b6063fffcc59723fe54559de1f5525e8c3
Omomayorwah <kintunde2010@gmail.com>
2025-05-07 08:51:03 +0100

    Update answers.sql

--- answers.sql
+++ answers.sql
@@ -1,26 +1,17 @@
--- An SQL query to create a table named CustomerOrder with two columns: OrderId and CustomerName
-CREATE TABLE CustomerOrder (
+-- An SQL query to create a table named ProductDetail with three columns: OrderId, CustomerName and Products
+CREATE TABLE ProductDetail (
     OrderId INT PRIMARY KEY,
-    CustomerName VARCHAR(100)
-);
--- An SQL query to insert three records into the CustomerOrder table
-INSERT INTO CustomerOrder (OrderId, CustomerName) 
-    VALUES (101,  'John Doe'),
-            (102, 'Jane Smith'),
-            (103, 'Emily Clark');
--- An SQL query to create a table named ProductDetails with two columns: OrderId and Products
-CREATE TABLE ProductDetails (
-    OrderId INT,
+    CustomerName VARCHAR(100),
     Products VARCHAR(100)
 );
--- An SQL query to insert six records into the ProductDetails table
-INSERT INTO ProductDetails (OrderId, Products)
-    VALUES (101, 'Laptop'),
-            (101, 'Mouse'),
-            (102, 'Tablet'),
-            (102, 'Keyboard'),
-            (102, 'Mouse'),
-            (103, 'Phone');
+-- An SQL query to insert six records into the ProductDetail table
+INSERT INTO ProductDetail (OrderId, CustomerName, Products)
+    VALUES (101, 'John Doe', 'Laptop'),
+            (101, 'John Doe', 'Mouse'),
+            (102, 'Jane Smith', 'Tablet'),
+            (102, 'Jane Smith', 'Keyboard'),
+            (102, 'Jane Smith', 'Mouse'),
+            (103, 'Emily Clark', 'Phone');
 -- An SQL query to create a table named CustomerOrder with two columns: OrderId and CustomerName
 CREATE TABLE CustomerOrder (
     OrderId INT PRIMARY KEY,
